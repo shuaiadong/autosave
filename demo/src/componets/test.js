83880bcc9f50d0613e054ea611061687bf09ed3a
v_zhonghuidong <v_zhonghuidong@baidu.com>
2019-05-21 20:21:46 +0800

    init

---
+++ demo/src/componets/test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Teat {
+  
+
+  render () {
+    return (
+      <div className="App">
+          <textarea value='1' />
+      </div>
+    );
+  }
+}
+
+export default Test;
